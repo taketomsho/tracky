f5d6e260b3b84424c3bec86a8b9463a8ce76c463
taketomsho <shotarotaketom@gmail.com>
2022-06-14 20:29:13 +0900

    新規ユーザー登録ができるように修正

--- apps/authentication/forms.py
+++ apps/authentication/forms.py
@@ -5,7 +5,7 @@ Copyright (c) 2019 - present AppSeed.us
 
 from django import forms
 from django.contrib.auth.forms import UserCreationForm,PasswordResetForm, SetPasswordForm
-from django.contrib.auth.models import User
+from .models import User
 
 
 class LoginForm(forms.Form):
@@ -56,7 +56,9 @@ class SignUpForm(UserCreationForm):
         ),
         label='Emailアドレス'
         )
-
+    class Meta:
+        model = User
+        fields = ('username', 'email', 'password1', 'password2')
 class VoltPasswordResetConfirmForm(SetPasswordForm):
     new_password1 = forms.CharField(
         widget=forms.PasswordInput(
