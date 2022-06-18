62ef454586fcace3d3ea39dfeb48727a6ef30602
taketomsho <shotarotaketom@gmail.com>
2022-06-18 19:48:03 +0900

    メール認証実装

--- apps/authentication/urls.py
+++ apps/authentication/urls.py
@@ -4,12 +4,14 @@ Copyright (c) 2019 - present AppSeed.us
 """
 
 from django.urls import path
-from .views import login_view, register_user, PasswordReset, PasswordResetDone, PasswordResetConfirm, PasswordResetComplete, EmailChange, EmailChangeDone, EmailChangeComplete, PasswordChange, PasswordChangeDone
+from .views import login_view, UserCreate, PasswordReset, PasswordResetDone, PasswordResetConfirm, PasswordResetComplete, EmailChange, EmailChangeDone, EmailChangeComplete, PasswordChange, PasswordChangeDone, UserCreateDone, UserCreateComplete
 from django.contrib.auth.views import LogoutView
 
 urlpatterns = [
     path('login/', login_view, name="login"),
-    path('register/', register_user, name="register"),
+    path('register/', UserCreate.as_view(), name="register"),
+    path('register/done', UserCreateDone.as_view(), name="user_create_done"),
+    path('register/complete/<token>/', UserCreateComplete.as_view(), name="user_create_complete"),
     path("logout/", LogoutView.as_view(), name="logout"),
     path('password_change/', PasswordChange.as_view(), name='password_change'),
     path('password_change/done/', PasswordChangeDone.as_view(), name='password_change_done'),
--- apps/authentication/views.py
+++ apps/authentication/views.py
@@ -47,29 +47,74 @@ def login_view(request):
     return render(request, "accounts/login.html", {"form": form, "msg": msg})
 
 
-def register_user(request):
-    msg = None
-    success = False
+class UserCreate(generic.CreateView):
+    """ユーザー仮登録"""
+    template_name = 'accounts/register.html'
+    form_class = SignUpForm
 
-    if request.method == "POST":
-        form = SignUpForm(request.POST)
-        if form.is_valid():
-            form.save()
-            username = form.cleaned_data.get("username")
-            raw_password = form.cleaned_data.get("password1")
-            user = authenticate(username=username, password=raw_password)
+    def form_valid(self, form):
+        """仮登録と本登録用メールの発行."""
+        # 仮登録と本登録の切り替えは、is_active属性を使うと簡単です。
+        # 退会処理も、is_activeをFalseにするだけにしておくと捗ります。
+        user = form.save(commit=False)
+        user.is_active = False
+        user.save()
 
-            msg = 'ユーザーが作成されました。<a href="/login">ログイン</a>しましょう。'
-            success = True
+        # アクティベーションURLの送付
+        current_site = get_current_site(self.request)
+        domain = current_site.domain
+        context = {
+            'protocol': self.request.scheme,
+            'domain': domain,
+            'token': dumps(user.pk),
+            'user': user,
+        }
 
-            # return redirect("/login/")
+        subject = render_to_string('accounts/mail_template/create/subject.txt', context)
+        message = render_to_string('accounts/mail_template/create/message.txt', context)
 
+        user.email_user(subject, message)
+        return redirect('user_create_done')
+
+class UserCreateDone(generic.TemplateView):
+    """ユーザー仮登録したよ"""
+    template_name = 'accounts/user_create_done.html'
+
+
+class UserCreateComplete(generic.TemplateView):
+    """メール内URLアクセス後のユーザー本登録"""
+    template_name = 'accounts/user_create_complete.html'
+    timeout_seconds = getattr(settings, 'ACTIVATION_TIMEOUT_SECONDS', 60*60*24)  # デフォルトでは1日以内
+
+    def get(self, request, **kwargs):
+        """tokenが正しければ本登録."""
+        token = kwargs.get('token')
+        try:
+            user_pk = loads(token, max_age=self.timeout_seconds)
+
+        # 期限切れ
+        except SignatureExpired:
+            return HttpResponseBadRequest()
+
+        # tokenが間違っている
+        except BadSignature:
+            return HttpResponseBadRequest()
+
+        # tokenは問題なし
         else:
-            msg = 'ユーザーを作成できません。'
-    else:
-        form = SignUpForm()
+            try:
+                user = User.objects.get(pk=user_pk)
+            except User.DoesNotExist:
+                return HttpResponseBadRequest()
+            else:
+                if not user.is_active:
+                    # 問題なければ本登録とする
+                    user.is_active = True
+                    user.save()
+                    return super().get(request, **kwargs)
 
-    return render(request, "accounts/register.html", {"form": form, "msg": msg, "success": success})
+        return HttpResponseBadRequest()
+
 
 
 class PasswordReset(PasswordResetView):
