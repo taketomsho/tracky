16b48403702971088e535ee62600347966863a3d
taketomsho <shotarotaketom@gmail.com>
2022-07-02 17:52:32 +0900

    mongodbを追加

--- core/settings.py
+++ core/settings.py
@@ -82,24 +82,29 @@ WSGI_APPLICATION = 'core.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
-if os.environ.get('DB_ENGINE') and os.environ.get('DB_ENGINE') == "mysql":
-    DATABASES = { 
-      'default': {
-        'ENGINE'  : 'django.db.backends.mysql', 
-        'NAME'    : os.getenv('DB_NAME'     , 'appseed_db'),
-        'USER'    : os.getenv('DB_USERNAME' , 'appseed_db_usr'),
-        'PASSWORD': os.getenv('DB_PASS'     , 'pass'),
-        'HOST'    : os.getenv('DB_HOST'     , 'localhost'),
-        'PORT'    : os.getenv('DB_PORT'     , 3306),
-        }, 
-    }
-else:
+
+mongo_url = os.environ.get('MONGODB_URI', 'mongodb://root:password@localhost/?authSource=admin')
+
+if os.environ.get('DB_ENGINE') and os.environ.get('DB_ENGINE') == "sqlite":
     DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.sqlite3',
             'NAME': 'db.sqlite3',
         }
     }
+else:
+    DATABASES = { 
+        'default': {
+            'ENGINE': 'djongo',
+            'NAME': 'test_database',
+            'CLIENT': {
+                'host': mongo_url,
+            }
+
+        }, 
+    }
+
+
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
